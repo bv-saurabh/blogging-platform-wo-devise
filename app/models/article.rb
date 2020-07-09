ef66151783209ebad430d0b5aff6cb7af79f73f3
Saurabh Jain <saurabh@blogvault.net>
2020-07-09 19:30:47 +0530

    Fix #1 : Adding all the files

---
+++ app/models/article.rb
@@ -0,0 +1,10 @@
+class Article < ApplicationRecord
+  belongs_to :user
+  validates :title, presence: true, length: { minimum: 5 }
+
+	def self.create(params, current_user)
+		param = {:title => params[:title], :body => params[:body], :user_id => current_user.id}
+		article = Article.new(param)
+		return article
+	end
+end
---
+++ config/routes.rb
@@ -0,0 +1,14 @@
+Rails.application.routes.draw do
+  get 'welcome/index'
+	root 'welcome#index'
+	
+	get '/login' => 'users#new_login'
+	post '/login' => 'users#create_login'
+	get '/logout' => 'users#logout'
+
+	get '/signup' => 'users#new_user'
+	post '/users' => 'users#create_user'
+
+	resources :articles
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
